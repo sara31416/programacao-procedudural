0e42ca6ae9bd1a5575906017acfead8ffb606a57
sara31416 <saraferreira31416@gmail.com>
2024-03-04 05:20:05 -0300

    lista 2 - Operadores lógicos e estrutura de decisão

---
+++ Listas/Lista2/exercicio2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+  public static void Main (string[] args)
+  {
+    Console.WriteLine ("Para saber a nota média do aluno siga os passos a seguir:");
+    
+    Console.WriteLine ("Digite a 1º nota do aluno:");
+    double nota1 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine ("Digite a 2º nota do aluno:");
+    double nota2 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine ("Digite a 3º nota do aluno:");
+    double nota3 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine ("Digite a 4º nota do aluno:");
+    double nota4 = double.Parse(Console.ReadLine());
+
+    double nota = (nota1 + nota2 + nota3 + nota4)/4;
+
+    Console.WriteLine("A nota média do aluno eh: " + nota);
+
+    if (nota >= 5.0)
+    {
+      Console.WriteLine("Aluno aprovado");
+    }
+    else if (nota < 5.0 && nota >=3.0)
+{
+  Console.WriteLine("Aluno de recuperacao");
+}
+    else 
+    {
+      Console.WriteLine("Aluno reprovado");
+    }
+  }
+}
---
+++ Listas/Lista2/exercicio3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program 
+{
+  public static void Main (string[] args) 
+  {
+    Console.WriteLine ("Digite o nome da criança:");
+    string nomeDaCriança = Console.ReadLine();
+
+    if (nomeDaCriança == "Filomena")
+    {
+      Console.WriteLine(" Rotina de Filomena\n 1. Escola das 07h as 12h\n 2.Almoço das 12h as 13h\n 3.Futbol das 14h as 16h\n 4.Dever de casa das 16h as 18h");
+       }
+    else if (nomeDaCriança == "Joselito")
+   {
+       Console.WriteLine("Rotina do Joselito\n 1. Escola das 07h as 12:30\n 2. Almoço das 13h as 14h\n 3. Natação das 14h as 16h\n 4. Reforço escolar das 16h as 19h");
+   }
+    else
+    {
+      Console.WriteLine("Nome invalido!");
+    }
+  }
+}
